28cb61203ab2a3e06d014cbc635538cc8d4f241f
Sai S. Bushigampala <95238483+saisbushigampala7@users.noreply.github.com>
2024-02-20 00:32:39 -0600

    Initial Commit
    Have lots of stuff to do, need to add dataset and then following website. Please work on it and I will add to it. Lmk when you want to access it.

---
+++ Pro1p1p2.py
@@ -0,0 +1,11 @@
+# Sai Bushigampala
+# Data Mining Pro 1 - P1 and P2
+# https://www.geeksforgeeks.org/ml-implementation-of-knn-classifier-using-sklearn/
+
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
+from sklearn.neighbors import KNeighborsClassifier 
+import matplotlib.pyplot as plt 
+
+
