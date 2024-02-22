7c23af077fb7527031fd0f528415ef8babd6d4e6
Sai S. Bushigampala <95238483+saisbushigampala7@users.noreply.github.com>
2024-02-21 21:30:10 -0600

    Isolated 90+/90- class labels.
    Trying to work on isolating data in attribute columns.

--- Pro1p1p2.py
+++ Pro1p1p2.py
@@ -1,6 +1,8 @@
 # Sai Bushigampala
 # Data Mining Pro 1 - P1 and P2
 # https://www.geeksforgeeks.org/ml-implementation-of-knn-classifier-using-sklearn/
+#https://www.activestate.com/resources/quick-reads/how-to-access-a-column-in-a-dataframe-using-pandas/#:~:text=You%20can%20use%20the%20loc,Let's%20see%20how.&text=If%20we%20wanted%20to%20access,in%20order%20to%20retrieve%20it.
+
 
 import numpy as np 
 import pandas as pd 
@@ -8,22 +10,21 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier 
 import matplotlib.pyplot as plt 
 
-
-def main():
     
     # reading file
-    training = r'C:\Users\SANG\Downloads\Testing dataset.xlsx'
-    train = pd.read_excel(training)
+training = r"C:\Users\ssbus\Downloads\Training dataset.xlsx"
+train = pd.read_excel(training)
     
-    testing = r'C:\Users\SANG\Downloads\Training dataset.xlsx'
-    test = pd.read_excel(testing)
+testing = r"C:\Users\ssbus\Downloads\Testing dataset.xlsx"
+test = pd.read_excel(testing)
+
+
+X=train.iloc[0].iloc[0]
     
+y=train.iloc[:,1]
+
+print(X)
     
+
     
-    
-    
-    
-    
-    
-if __name__ == "__main__":
-        main()
+
